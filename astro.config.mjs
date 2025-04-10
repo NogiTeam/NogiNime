420616313ff27c151b2c2fad164622ce133666dd
NogiTeam <akunnyasoeltan3@gmail.com>
2025-04-10 09:19:09 +0700

    main

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,23 @@
-ù// @ts-check
+// @ts-check
 import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
-
+import sitemap from '@astrojs/sitemap';
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   env: {
     schema: {
+      site: 'https://example.com',
+  integrations: [
+    sitemap({
+      filter: (page) => {
+        const allowedPages = ['/', '/ongoing', '/completed', 'anime'];
+        return allowedPages.includes(page);
+      }
+    })
+  ]
       API_URL: envField.string({
         access: "public",
         context: "server",
